4f762e92ca168a02fbf4cd5b321f5a3aec3a2370
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-08 18:53:45 +0100

    Banner styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,7 +17,9 @@ function Banner() {
     fetchData();
   }, []);
 
-  console.log(movie);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
     <header
@@ -31,7 +33,16 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h2 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h2>
       </div>
     </header>
   );
