92601730a225353be7ad1a7b9c06a0a115f99daf
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-08 20:06:47 +0100

    Navbar styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import netflixLogo from "../images/netflix-logo.png";
 import avatar from "../images/netflix-avatar.png";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav--black"}`}>
       <img className="nav__logo" src={netflixLogo} alt="Netflix logo" />
       <img className="nav__avatar" src={avatar} alt="User Icon" />
     </div>
