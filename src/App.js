4659eb997e103ae332cdad5db1e5dc936e24e906
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-08 15:16:48 +0100

    Create row components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Row from './components/Row';
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>React Netflix Clone</h1>
-     
+      <Row title="NETFLIX ORIGINALS" />     
+      <Row title="Trending Now" />     
     </div>
   );
 }
