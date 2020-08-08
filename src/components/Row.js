b0c350b4a15c9ce459db0bd59baf2f9ca000c0ad
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-08 18:13:18 +0100

    Added isLargeRow prop and styling

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,7 +3,7 @@ import axios from "../utils/axios";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // Code inside useEffect will run based on specific conditions.
@@ -28,8 +28,10 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow ? "row__poster-large" : ""}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
